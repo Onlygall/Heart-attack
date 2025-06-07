fe95207b6e9bff2e07112a6e893744b7aa293d82
Galeh Saputra <gall@students.unnes.ac.id>
2025-06-08 01:32:08 +0700

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Qf8bHt6ICWu6VTJErEztf8RdC4Weys3D
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
