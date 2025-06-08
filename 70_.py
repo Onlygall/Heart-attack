7e2e042837f2189f90448628aa51a6412c3e5be2
Galeh Saputra <gall@students.unnes.ac.id>
2025-06-09 01:51:05 +0700

    Update 70_.py

--- 70_.py
+++ 70_.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/18hvFoCgL7Ap0OtJ8nPTWm2TIP3XXQHV0
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
