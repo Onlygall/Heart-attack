8b94c6e4cac0eb93d6de484d0cd2d7315d48e61f
Galeh Saputra <gall@students.unnes.ac.id>
2025-06-09 18:43:30 +0700

    Update 75_.py

--- 75_.py
+++ 75_.py
@@ -401,10 +401,8 @@ elif page == "🛠️ Modeling":
     cluster_risk_map = risk_summary["risk_label"].to_dict()
     df_clustered["risk_level"] = df_clustered["cluster"].map(cluster_risk_map)
 
-    st.markdown("### 📈 Distribusi Risiko per Klaster")
-    st.dataframe(risk_summary.style.format({"mean": "{:.2%}"}))
 
-    st.markdown("### 🧭 Visualisasi Klaster (PCA)")
+    st.markdown("**🧭 Visualisasi Klaster (PCA)"**)
     pca = PCA(n_components=2)
     X_pca = pca.fit_transform(X_cluster_processed)
 
@@ -416,6 +414,9 @@ elif page == "🛠️ Modeling":
     ax_pca.set_xlabel("PCA 1")
     ax_pca.set_ylabel("PCA 2")
     st.pyplot(fig_pca)
+    
+    st.markdown("**📈 Distribusi Risiko per Klaster**")
+    st.dataframe(risk_summary.style.format({"mean": "{:.2%}"}))
 
 
 # ===============================
