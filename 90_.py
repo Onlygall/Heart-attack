423f4c9a38ac3f16cb1e0d46d65a95783bbe91ed
Galeh Saputra <gall@students.unnes.ac.id>
2025-06-12 00:04:50 +0700

    Update 90_.py

--- 90_.py
+++ 90_.py
@@ -11,6 +11,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
 import seaborn as sns
 import joblib
 from joblib import load
